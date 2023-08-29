b43373b240de5e05d48d505c59ace20d31cc7cb1
Marcelo Garcia <marcelomgarcia@gmail.com>
2023-08-29 20:30:29 +0300

    Using dotenv

--- sample_analyze_layout.py
+++ sample_analyze_layout.py
@@ -5,17 +5,15 @@ The async versions of the samples require Python 3.6 or later.
 To learn more, please visit the documentation - Quickstart: Form Recognizer Python client library SDKs
 https://docs.microsoft.com/en-us/azure/applied-ai-services/form-recognizer/quickstarts/try-v3-python-sdk
 """
-
+import os
+from dotenv import load_dotenv
 from azure.core.credentials import AzureKeyCredential
 from azure.ai.formrecognizer import DocumentAnalysisClient
 
-"""
-Remember to remove the key from your code when you're done, and never post it publicly. For production, use
-secure methods to store and access your credentials. For more information, see 
-https://docs.microsoft.com/en-us/azure/cognitive-services/cognitive-services-security?tabs=command-line%2Ccsharp#environment-variables-and-application-configuration
-"""
-endpoint = "YOUR_FORM_RECOGNIZER_ENDPOINT"
-key = "YOUR_FORM_RECOGNIZER_KEY"
+load_dotenv()
+
+endpoint = os.environ["ENDPOINT"]
+key = os.environ["KEY"]
 
 # sample document
 formUrl = "https://raw.githubusercontent.com/Azure-Samples/cognitive-services-REST-api-samples/master/curl/form-recognizer/sample-layout.pdf"
